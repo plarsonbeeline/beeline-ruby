de65bfb785f9e52a9fa19e917b6a9e3886d29bd0
Phil Larson <phil.larson@icloud.com>
2018-01-24 09:23:27 -0800

    Switch to attr_reader

--- app/models/financial_report.rb
+++ app/models/financial_report.rb
@@ -1,13 +1,13 @@
 class FinancialReport
   def initialize(user_id: nil, currency: Money.default_currency, after_at: nil, counts: {}, amounts: {})
-    self.user_id = user_id
-    self.currency = Money::Currency.wrap(currency)
-    self.after_at = after_at
-    self.counts = counts
-    self.amounts = amounts
+    @user_id = user_id
+    @currency = Money::Currency.wrap(currency)
+    @after_at = after_at
+    @counts = counts
+    @amounts = amounts
   end
 
-  attr_accessor :user_id, :currency, :after_at, :counts, :amounts
+  attr_reader :user_id, :currency, :after_at, :counts, :amounts
 
   def count(k)
     counts[k] || 0
